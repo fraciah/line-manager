fd0f4dd4f283e18e52fed2969d03394f8756177c
fraciah <karfraciah@gmail.com>
2024-06-15 00:03:30 +0300

    Add a hook for fetching a sub collection

---
+++ src/hooks/useSubCollection.jsx
@@ -0,0 +1,32 @@
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+
+const useSubCollection = (collectionName, id, subCol) => {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const getSingleData = () => {
+      if (id) {
+        const postRef = query(collection(db, collectionName, id, subCol));
+        onSnapshot(postRef, (snapshot) => {
+          setData(
+            snapshot.docs.map((doc) => ({
+              ...doc.data(),
+              id: doc.id,
+            }))
+          );
+          setLoading(false);
+        });
+      }
+    };
+    getSingleData();
+  }, [id, collectionName, subCol]);
+  return {
+    data,
+    loading,
+  };
+};
+
+export default useSubCollection;
